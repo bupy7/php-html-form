d7ff13724d96cd8bc36828423ef4fae63b626b3c
Vasily Belosloodcev <vasily.belosloodcev@gmail.com>
2022-01-30 14:49:54 +0500

    refactoring test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,14 @@
 <?php
 
 if (version_compare(PHP_VERSION, '7.1.0', '>=')) {
+    require_once __DIR__ . '/Php71TestCase.php';
+
     class TestCase extends Php71TestCase
     {
     }
 } else {
+    require_once __DIR__ . '/Php56TestCase.php';
+
     class TestCase extends Php56TestCase
     {
     }
