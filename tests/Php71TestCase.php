b064b2e0642a3e1f3ae0514fe2e3e0cc85a0c9a9
Vasily Belosloodcev <vasily.belosloodcev@gmail.com>
2022-01-30 14:53:41 +0500

    fixed test case

--- tests/Php71TestCase.php
+++ tests/Php71TestCase.php
@@ -20,11 +20,19 @@ class Php71TestCase extends BaseTestCase
 
     public static function assertRegExp(string $pattern, string $string, string $message = ''): void
     {
-        self::assertMatchesRegularExpression($pattern, $string, $message);
+        if (method_exists(self::class, 'assertMatchesRegularExpression')) {
+            self::assertMatchesRegularExpression($pattern, $string, $message);
+        } else {
+            parent::assertRegExp($pattern, $string, $message);
+        }
     }
 
     public static function assertNotRegExp(string $pattern, string $string, string $message = ''): void
     {
-        self::assertDoesNotMatchRegularExpression($pattern, $string, $message);
+        if (method_exists(self::class, 'assertDoesNotMatchRegularExpression')) {
+            self::assertDoesNotMatchRegularExpression($pattern, $string, $message);
+        } else {
+            parent::assertNotRegExp($pattern, $string, $message);
+        }
     }
 }
